71be6365af23f4b5a847140497100ee52616c872
songlj233 <457281690@qq.com>
2023-01-04 22:26:12 +0800

    保存请求记录到数据库中

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,9 +1,18 @@
 import { Configuration, OpenAIApi } from "openai";
+const mysql = require('mysql');
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+
+const pool = mysql.createPool({
+  connectionLimit : 10,
+  host: '8.130.34.204',
+  user: 'root',
+  password: 'yourpassword',     // 改成你自己的密码
+  database: 'chatgpt'    // 改成你的数据库名称
+});
 
 export default async function (req, res) {
   const completion = await openai.createCompletion({
@@ -15,18 +24,24 @@ export default async function (req, res) {
     presence_penalty: 0.2,
     max_tokens: 1000
   });
+  record(req,res,completion);
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function record(req,res,completion) {
+  console.log("ip:", req.ip);
+  console.log("body:", req.body);
+  console.log("question:" + req.body.question);
+  console.log("answer:", completion.data.choices[0].text);
+  var  insertSql = 'INSERT INTO invoke_record(ip, prompt, completion) VALUES(?,?,?)';
+  var  insertParams = [req.ip, req.body.question, completion.data.choices[0].text];
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  //增
+  pool.query(insertSql, insertParams, function (err, result) {
+      if(err){
+          console.log('[INSERT ERROR] - ',err.message);
+          return;
+      }
+      console.log('[保存记录成功]');
+  });
 }
